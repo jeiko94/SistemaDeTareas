5beebef8ce35c58684b6767045a52f5da12dc8f2
Yeisson Villamil <yeisson.villamil@globalmvm.com>
2025-01-18 13:09:20 -0500

    Arquitectura Hexagonal y Principios SOLID:
    Single Responsibility: Cada servicio y controlador enfocado en su rol.
    Open/Closed: Podemos extender la funcionalidad sin modificar partes esenciales.
    Liskov Substitution, Interface Segregation, Dependency Inversion: Aplicados al depender de interfaces (IUsuarioRepository.).

--- SistemaDeTareas.Api/Program.cs
+++ SistemaDeTareas.Api/Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaDeTareas.Aplicacion.Interfaces;
+using SistemaDeTareas.Aplicacion.Services;
+using SistemaDeTareas.Domain.Interfaces;
+using SistemaDeTareas.Infraestructura.Persistence;
+using SistemaDeTareas.Infraestructura.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Configurar EF Core con SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Registrar Repositorios
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<ITareaRepository, TareaRepository>();
+
+// Registrar Servicios
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<ITareaService, TareaService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
